e1c6d94c9e7f67bbb966ac19362bc702d002091a
popoiupeer <erdand160307@gmail.com>
2025-04-16 09:07:44 +0700

    Update hwto22.kt

--- hwto22.kt
+++ hwto22.kt
@@ -1,82 +1,61 @@
 using System;
-using System.Collections.Generic;
-public interface INotificationObserver
+
+public interface ICommand
 {
-    void Update(string message);
+    void Execute();
 }
-public sealed class NotificationService
+
+public class Message
 {
-    private static NotificationService _instance;
-    private static readonly object _lock = new object();
-    private List<INotificationObserver> _observers = new List<INotificationObserver>();
-    private NotificationService() { }
+    public static string text { get; set; } = string.Empty; 
+}
 
-    public static NotificationService Instance
+public class Cin : ICommand
+{
+    public void Execute()
     {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance == null)
-                    _instance = new NotificationService();
-                return _instance;
-            }
-        }
-    }
-    public void Subscribe(INotificationObserver observer)
-    {
-        _observers.Add(observer);
-    }
-
-    public void Unsubscribe(INotificationObserver observer)
-    {
-        _observers.Remove(observer);
-    }
-
-    public void Notify(string message)
-    {
-        foreach (var observer in _observers)
-            observer.Update(message);
+        Console.Write("Введите строку: ");
+        Message.text += Console.ReadLine(); 
     }
 }
 
-public class MobileAppUI : INotificationObserver
+public class DeleteLastSign : ICommand
 {
-    public void Update(string message)
+    public void Execute()
     {
-        Console.WriteLine($"UI: Показать уведомление - '{message}'");
+        Message.text = Message.text.Remove(Message.text.Length - 1); 
+        Console.WriteLine("Текст: " + Message.text); 
     }
 }
 
-public class TelegramBot : INotificationObserver
+public class TextRedactor
 {
-    public void Update(string message)
-    {
-        Console.WriteLine($"Telegram: Отправлено сообщение - '{message}'");
-    }
-}
+    private ICommand command;
 
-public class Logger : INotificationObserver
-{
-    public void Update(string message)
+    public void SetCommand(ICommand command)
     {
-        Console.WriteLine($"Log: Записано в журнал - '{message}'");
+        this.command = command;
+    }
+
+    public void ExecuteThis()
+    {
+        command.Execute();
     }
 }
 
 class Program
 {
     static void Main()
-    {       
-        var notificationService = NotificationService.Instance;
-        var ui = new MobileAppUI();
-        var telegram = new TelegramBot();
-        var logger = new Logger();
-        notificationService.Subscribe(ui);
-        notificationService.Subscribe(telegram);
-        notificationService.Subscribe(logger);
-        notificationService.Notify("Новое сообщение от системы!");
-        notificationService.Unsubscribe(logger);
-        notificationService.Notify("Обновление данных завершено!");
+    {
+        ICommand addSymbol = new Cin();
+        ICommand deleteLastSign = new DeleteLastSign();
+
+        TextRedactor remote = new TextRedactor();
+
+        remote.SetCommand(addSymbol);
+        remote.ExecuteThis();
+
+        remote.SetCommand(deleteLastSign);
+        remote.ExecuteThis();
     }
 }
